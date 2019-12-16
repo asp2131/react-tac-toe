cd65a3b85cca32aa3fca74b63935ea84ede84c79
asp2131 <akinpds@gmail.com>
2019-12-16 11:02:36 -0600

    HandleClick is not defined

--- src/App.js
+++ src/App.js
@@ -19,9 +19,17 @@ class Game extends React.Component {
 }
 
 class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null
+    }
+  }
   render() {
     return (
-      <button className="square" onClick={() => console.log(this.props.value) }>
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick() }>
         {this.props.value} 
       </button>
     );
@@ -29,8 +37,19 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square 
+              value={this.state.squares[i]}
+              onClick={this.handleClick(i)}
+            />;
   }
 
   render() {
